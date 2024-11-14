09a5aaea9aa1418405409b49b873c6911d67bb53
rodrigocastromo <37327445-rodrigocastromo@users.noreply.replit.com>
2024-11-14 19:34:51 +0000

    Agent query: Is the loading spinner showing up when searching for a user by CPF?

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+// Utility functions for spinner modal
+function showSpinner() {
+    document.getElementById('spinnerModal').style.display = 'block';
+}
+
+function hideSpinner() {
+    document.getElementById('spinnerModal').style.display = 'none';
+}
